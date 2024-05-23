0ece78943bf1a0177adc6a8195c7f65da27decef
Bisbeedog <nicholasprescottbisbee@gmail.com>
2024-05-22 21:12:05 -0700

    Update data.js

--- data.js
+++ data.js
@@ -122,7 +122,7 @@ var data = {
     Race: [
         {
             Name: "Bahrain Grand Prix",
-            Date: "2023-03-02",
+            Date: "2024-03-02",
             Finish: [1, 11, 55, 16, 63, 4, 44, 81, 14, 18, 24, 20, 3, 22, 23, 27, 31, 10, 77, 2],
             Picks: [
                 { Player: "Nick", Drivers: [4, 44, 22] },
@@ -136,7 +136,7 @@ var data = {
            },
            {
             Name: "Saudi Arabian Grand Prix",
-            Date: "2023-03-09",
+            Date: "2024-03-09",
             Finish: [1, 11, 16, 81, 14, 63, 38, 4, 44, 27, 23, 20, 31, 22, 2, 3, 77, 24, 18, 10],
             Picks: [
                 { Player: "Nick", Drivers: [4, 44, 22] },
@@ -151,7 +151,7 @@ var data = {
             },
             {
              Name: "Australian GP",
-             Date: "2023-03-23",
+             Date: "2024-03-23",
              Finish: [55, 16, 4, 81, 11, 18, 22, 14, 27, 20, 23, 3, 10, 77, 24, 31, 63, 44, 1, 2],
              Picks: [
                  { Player: "Nick", Drivers: [4, 22, 44] },
@@ -166,7 +166,7 @@ var data = {
              },
              {
               Name: "Japanese GP",
-              Date: "2023-04-06",
+              Date: "2024-04-06",
               Finish: [1, 11, 55, 16, 4, 14, 63, 81, 44, 22, 27, 18, 20, 77, 31, 10, 2, 24, 3, 23],
               Picks: [
                   { Player: "Nick", Drivers: [4, 44, 22] },
@@ -181,7 +181,7 @@ var data = {
               },
               {
                 Name: "Chinese GP Sprint",
-                Date: "2023-04-20",
+                Date: "2024-04-20",
                 Finish: [1, 44, 11, 16, 55, 4, 81, 63, 24, 20, 3, 77, 31, 18, 10, 22, 23, 2, 27, 14],
                 Picks: [
                     { Player: "Nick", Drivers: [44, 4, 22] },
@@ -195,7 +195,7 @@ var data = {
                 },
               {
                Name: "Chinese GP",
-               Date: "2023-04-21",
+               Date: "2024-04-21",
                Finish: [1, 4, 11, 16, 55, 63, 14, 81, 44, 27, 31, 23, 10, 24, 18, 20, 2, 3, 22, 77],
                Picks: [
                    { Player: "Nick", Drivers: [4, 44, 22] },
@@ -210,7 +210,7 @@ var data = {
                },
                {
                  Name: "Miami GP Sprint",
-                 Date: "2023-05-04",
+                 Date: "2024-05-04",
                  Finish: [1, 16, 11, 3, 55, 81, 27, 22, 10, 2, 24, 63, 23, 77, 31, 44, 14, 20, 18, 4],
                  Picks: [
                      { Player: "Nick", Drivers: [22, 44, 4] },
@@ -224,7 +224,7 @@ var data = {
                  },
                  {
                   Name: "Miami GP",
-                  Date: "2023-05-05",
+                  Date: "2024-05-05",
                   Finish: [4, 1, 16, 11, 55, 44, 22, 63, 14, 31, 27, 10, 81, 24, 3, 77, 18, 23, 20, 2],
                   Picks: [
                       { Player: "Nick", Drivers: [4, 44, 22] },
@@ -239,7 +239,7 @@ var data = {
                   },
                   {
                    Name: "Emilia Romagna GP",
-                   Date: "2023-05-05",
+                   Date: "2024-05-17",
                    Finish: [1, 4, 16, 81, 55, 44, 63, 11, 18, 22, 27, 20, 3, 31, 24, 10, 2, 77, 14, 23],
                    Picks: [
                        { Player: "Nick", Drivers: [4, 44, 22] },
@@ -247,7 +247,7 @@ var data = {
                        { Player: "DOD", Drivers: [1, 63, 22] },
                        { Player: "Logan", Drivers: [55, 44, 23] },
                        { Player: "Vyanka", Drivers: [81, 55, 63] },
-                       { Player: "Pud", Drivers: [16, 55, d] }
+                       { Player: "Pud", Drivers: [16, 55, 10] }
                    ],
                    BonusPoints: "\n Magnussen: +2 points for most places gained"
        
